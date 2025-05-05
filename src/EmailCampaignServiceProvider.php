4f58a070642c3cfe31a69d9b4abd962a7939bf04
Ali Hadi <rajanihadi17@gmail.com>
2025-05-05 20:58:34 +0500

    Fix

--- src/EmailCampaignServiceProvider.php
+++ src/EmailCampaignServiceProvider.php
@@ -9,16 +9,13 @@ class EmailCampaignServiceProvider extends ServiceProvider
 
     public function register()
     {
-        // $this->mergeConfigFrom(__DIR__ . '/../config/email-campaign.php', 'email-campaign');
+        
     }
 
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->publishes([
-            __DIR__.'/../config/email-campaign.php' => config_path('email-campaign.php'),
-        ]);
         $this->publishes([
             __DIR__.'/../database/migrations' => database_path('migrations'),
         ], 'email-campaign-migrations');
